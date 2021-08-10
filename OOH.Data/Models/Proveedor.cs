d230ffcc9946969082c29ec5a5fd193d991dc881
Rafael Mendoza <rafaelmendoza@vivaoutdoor.com>
2021-08-10 17:57:02 -0600

    -se agrego comando para crear un nuevo proveedor al proyecto de definicion

---
+++ OOH.Data/Models/Proveedor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOH.Data.Models
+{
+    /// <summary>
+    /// Modelo de la base de proveedor
+    /// </summary>
+    public class Proveedor
+    {
+    }
+}
