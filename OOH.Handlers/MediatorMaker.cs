73eaaa435560793d0580186d4b72fe2207b43586
jose.rivas <jose.rivas@IT-03>
2021-08-09 18:12:54 -0600

    Agregar archivos de proyecto.

---
+++ OOH.Handlers/MediatorMaker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOH.Handlers
+{
+    /// <summary>
+    /// This is a helper class to map mediator commands and queries with their implementations since only the assembly a single public class is needed 
+    /// </summary>
+    public class MediatorMaker
+    {
+    }
+}
---
+++ OOH.WebApi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OOH.WebApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
