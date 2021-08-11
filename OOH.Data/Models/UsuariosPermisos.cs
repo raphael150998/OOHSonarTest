68cf7dc5b05b986e52f5f52100cc63f6bd233579
Jose Rivas <joserivas@vivaoutdoor.com>
2021-08-11 10:50:23 -0600

    Clases de usuarios y perfiles

---
+++ OOH.Data/Models/UsuariosPermisos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OOH.Data.Models
+{
+    public class UsuariosPermisos
+    {
+        public int Id { get; set; }
+        public int PerfilId { get; set; } 
+        public int ModuloId { get; set; }
+        public Plataformas PlataformaId { get; set; }
+    
+        [Required]
+        [StringLength(50)]
+        public string Permiso { get; set; }
+        public bool Ejecutar { get; set; }
+        public bool Ver { get; set; }
+        public bool Agregar { get; set; }
+        public bool Modificar { get; set; }
+        public bool Eliminar { get; set; }
+
+    }
+
+    public enum Plataformas
+    {
+        MultiPlataforma = 1,
+        Web = 2,
+        Movil = 3
+    }
+}
