8e888ede8d34c8270e9089e5e1607fbcf055746d
Jose Rivas <joserivas@vivaoutdoor.com>
2021-09-06 15:47:01 -0600

    jquery Validate ,Agencias se le quito una parte

--- OOH.WebApi/wwwroot/js/jquery.ValidateHelper.js
+++ OOH.WebApi/wwwroot/js/jquery.ValidateHelper.js
@@ -1,7 +1,16 @@
 ﻿var Validate = {
 
     Form: function (identify, config) {
-        
+
+        config.errorPlacement = function (error, element) {
+            var placement = $(element).data('error');
+            if (placement) {
+                $(placement).append(`<i data-toggle="tooltip" data-placement="top" data-trigger="hover" title="${error.text()}" class="fas fa-exclamation-circle error"></i>`);
+            } else {
+                $(element).parent().append(`<i data-toggle="tooltip" data-placement="top" data-trigger="hover" title="${error.text()}" class="fas fa-exclamation-circle error"></i>`);
+                //error.insertAfter(element);
+            }
+        };
         $(identify).validate(config);
 
     }
