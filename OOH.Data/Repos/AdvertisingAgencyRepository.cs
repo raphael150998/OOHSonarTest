543170ff30cf61844fac5a7153229b2a48c04f9f
Rafael Mendoza <rafaelmendoza@vivaoutdoor.com>
2021-09-07 10:47:50 -0600

    finalizacion de validate global

--- OOH.Data/Repos/AdvertisingAgencyRepository.cs
+++ OOH.Data/Repos/AdvertisingAgencyRepository.cs
@@ -22,7 +22,7 @@ namespace OOH.Data.Repos
 
         public async Task<int> Create(AgenciasPublicidad agencia)
         {
-            string sql = "INSERT INTO AgenciasPublicidad(Nombre, Comision, Activo) values (@Nombre, @Comision, @Activo)";
+            string sql = "INSERT INTO AgenciasPublicidad(Nombre, Comision, Activo) VALUES (@Nombre, @Comision, @Activo)";
             var id = await PostData(sql, true, new DynamicParameters(agencia), false, _connectionString);
             return id;
         }
@@ -34,7 +34,7 @@ namespace OOH.Data.Repos
 
         public async Task Remove(int id)
         {
-            await PostData($"Update AgenciasPublicidad set Activo = false where AgenciaId = {id}", false, null, false, _connectionString);
+            await PostData($"UPDATE AgenciasPublicidad SET Activo = false WHERE AgenciaId = {id}", false, null, false, _connectionString);
         }
 
         public async Task<IEnumerable<AgenciasPublicidad>> Select(string _Where = "")
@@ -44,7 +44,7 @@ namespace OOH.Data.Repos
 
         public async Task Update(AgenciasPublicidad agencia)
         {
-            string sql = "UPDATE AgenciasPublicidad SET Nombre = @Nombre, Comision = @Comision, Activo = @Activo";
+            string sql = "UPDATE AgenciasPublicidad SET Nombre = @Nombre, Comision = @Comision, Activo = @Activo WHERE AgenciaId = @AgenciaId";
 
             await PostData(sql, true, new DynamicParameters(agencia), false, _connectionString);
         }
