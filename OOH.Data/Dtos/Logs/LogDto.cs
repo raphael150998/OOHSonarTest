b1c663b74de17f4c524cb8de1e7e77a9588c50b8
Rafael Mendoza <rafaelmendoza@vivaoutdoor.com>
2021-10-06 15:10:24 -0600

    se agrego al repo de sitios el metodo para obtener listado de sitios para select2.js

---
+++ OOH.Data/Dtos/Logs/LogDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOH.Data.Dtos.Logs
+{
+    /// <summary>
+    /// Objeto de transferencia de datos de log para cuando se requiere pasar mensajes personalizados
+    /// </summary>
+    public class LogDto
+    {
+        /// <summary>
+        /// descripcion personalizada del log
+        /// </summary>
+        public string Descripcion { get; set; }
+
+        public Platform? PlatformId { get; set; }
+
+        public string Entidad { get; set; }
+
+        public long EntidadId { get; set; }
+
+        public string Version { get; set; }
+    }
+}
